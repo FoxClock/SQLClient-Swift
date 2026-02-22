6f1f0dcc76e42790f598606d3d0bc918bc85c03b
ClockFox <44942917+FoxClock@users.noreply.github.com>
2026-02-22 15:08:05 +1100

    Address issue with CI pipeline failure

--- Sources/CFreeTDS/include/CFreeTDS.h
+++ Sources/CFreeTDS/include/CFreeTDS.h
@@ -4,8 +4,13 @@
 #pragma once
 
 #ifdef __APPLE__
+    #ifdef __x86_64__
     #include "/usr/local/opt/freetds/include/sybdb.h"
     #include "/usr/local/opt/freetds/include/sybfront.h"
+    #else
+    #include "/opt/homebrew/opt/freetds/include/sybdb.h"
+    #include "/opt/homebrew/opt/freetds/include/sybfront.h"
+    #endif
 #else
     #include <sybdb.h>
     #include <sybfront.h>
